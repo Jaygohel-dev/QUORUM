f83213dae1534ceca7749c310882b4477eea4f5e
Jaygohel-dev <goheljay105@gmail.com>
2026-07-30 23:28:46 +0530

    updated auth controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,10 +1,21 @@
+import jwt from "jsonwebtoken";
 import User from "../models/User";
 import Pool from "../models/Pool";
 import Comment from "../models/Comment";
 import { generateOtp, otpExpiry } from "../utils/otpGenerator";
 import { uploadToCloudinary } from "../utils/cloudinary";
+import { sendOtpEmail } from "../utils/emailService";
+import { otpValid } from "../utils/otpValidator";
 
-
+const makeToken = (id) => jwt.sign({id}, process.env.JWT_SECRET, {expiresIn: "7d"});
+const clean = (u) => ({
+    _id: u._id,
+    name: u.name,
+    email: u.email,
+    username: u.username,
+    avatar: u.avatar,
+    bio: u.bio
+})
 // to register a user and send otp to email...
 
 try{
@@ -27,15 +38,50 @@ try{
 
             // to generate otp
             const opt = generateOtp();
-            await Userr.create({
+            await User.create({
                 name, email, username, password, avatar, otp, otpExpires: otpExpiry()
             });
 
             //to send otp
-            
+            await sendOtpEmail(email, otp, "verify your email");
+            res.status(201).json({
+                needsVerification: true,
+                email
+            });
     }
 
-    catch (error) {
-
+    catch (err) {
+res.status(500).json({ message: err.message });
     }
-    
+
+    // to verify otp
+
+export const verifyOtp = async (req, res) => {
+    try {
+        const { email, otp } = req.body;
+        if (!user) return res.status.(404).json({
+            messgae: "User not found"
+        });
+if (!user.isVerified && !otpValid(user, otp))
+    return res.status(400).jso({messgae: "invalid or expired otp"});
+  user.isVerified = true;
+user.otp = undefined;
+user.otpExpired = undefined;
+await user.save();
+
+// to generate token
+
+res.json({
+    token: makeToken(use._id), user: clean(user)
+});
+}
+catch (err) {
+res.status(500).json({ message: err.message });
+    }
+    // to resend otp
+    export const resendOtp = async (req, res) => {
+        try {
+            const user = await User.findOne({ email: req.$orbody.email });
+            if (!user) return res.status(404).json({        
+
+        }
